35ce198aaed7450c775475d446e3d1157d2a863e
meera-rm <raamesh.meera@gmail.com>
2022-08-28 22:52:49 -0400

    package

--- backend/queries/stocks.js
+++ backend/queries/stocks.js
@@ -24,8 +24,8 @@ const createStock = async (stock) => {
   //determine if the stock is healthy or not
   try {
     return await db.any(
-      'INSERT INTO stocks (chart_name, equity_name, metrics) VALUES ($1, $2, $3) RETURNING *',
-      [stock.chart_name, stock.equity_name, stock.metrics]
+      'INSERT INTO stocks (chart_name, equity_name, metrics,users) VALUES ($1, $2, $3, $4) RETURNING *',
+      [stock.chart_name, stock.equity_name, stock.metrics, stock.users]
     );
   } catch (error) {
     console.log(error.message || error);
@@ -33,18 +33,17 @@ const createStock = async (stock) => {
   }
 };
 
-
-const updateStock = async (id,{chart_name, equity_name, metrics} ) => {
-    try {
-      const stock = await db.one(
-        "UPDATE stocks SET chart_name=$1, equity_name=$2, metrics=$3 where id=$4 RETURNING *",
-        [chart_name, equity_name, metrics, id]
-      );
-      return stock;
-    } catch (error) {
-      return error;
-    }
-  };
+const updateStock = async (id, { chart_name, equity_name, metrics }) => {
+  try {
+    const stock = await db.one(
+      'UPDATE stocks SET chart_name=$1, equity_name=$2, metrics=$3 where id=$4 RETURNING *',
+      [chart_name, equity_name, metrics, id]
+    );
+    return stock;
+  } catch (error) {
+    return error;
+  }
+};
 
 const deleteStock = async (id) => {
   try {
