79fa9a007b0aca25da9705f8ab164ccf3f1d8b4c
meera-rm <raamesh.meera@gmail.com>
2022-09-06 09:22:41 -0400

    fixed map

--- frontend/src/Components/index/Charts.js
+++ frontend/src/Components/index/Charts.js
@@ -1,14 +1,12 @@
 import { useState, useEffect } from 'react';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Table from 'react-bootstrap/Table';
-import '../../styles/index.css'
-
+import '../../styles/index.css';
 
 const API = process.env.REACT_APP_API_URL;
 
 const Charts = () => {
- 
   const [charts, setCharts] = useState([]);
   // const [flag, setFlag] = useState(false);
 
@@ -19,33 +17,28 @@ const Charts = () => {
     });
   }, []);
 
-
   return (
-    <div className='charts' >
-     
+    <div className='charts'>
       <h1 className='title-charts'>Chart</h1>
       <Table
-       className='table-container'
-       stripped='true'
-       bordered
-       hover
-       responsive='sm'
+        className='table-container'
+        stripped='true'
+        bordered
+        hover
+        responsive='sm'
       >
         <thead>
           <tr className='green'>
-            <th scope="col">ID</th>
-            <th scope="col"> Equity Name</th>
-            <th scope="col">Chart Name</th>
-            <th scope="col">Metrics</th>
-            <th scope="col">Users</th>
-            <th scope="col">Action</th>
+            <th scope='col'>ID</th>
+            <th scope='col'> Equity Name</th>
+            <th scope='col'>Chart Name</th>
+            <th scope='col'>Metrics</th>
+            <th scope='col'>Users</th>
+            <th scope='col'>Action</th>
           </tr>
         </thead>
         <tbody>
-          
-          {charts.map((chart) => {
-
-           
+          {charts?.map((chart) => {
             return (
               <tr className='' key={chart.id}>
                 <td>
@@ -91,22 +84,19 @@ const Charts = () => {
                 </td>
 
                 <td>
-                  
-
                   <Link
                     style={{ color: 'black' }}
                     // to={{pathName: `/charts/data`,
                     //     state:{chart}}}
                     // to={passParam}
-                     to= {{pathname: `/charts/data`,
-                      search:`?metrics=${chart.metrics}&equity_name=${chart.equity_name}&chart_name=${chart.chart_name}`,
+                    to={{
+                      pathname: `/charts/data`,
+                      search: `?metrics=${chart.metrics}&equity_name=${chart.equity_name}&chart_name=${chart.chart_name}`,
                     }}
                   >
                     View Charts
                   </Link>
-
                 </td>
-
               </tr>
             );
           })}
